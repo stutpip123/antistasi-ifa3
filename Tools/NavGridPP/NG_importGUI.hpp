06cf07ec7f986a51ef664988cf53e730b64b655d
Caleb Serafin <calebserafin@outlook.com>
2021-02-16 17:16:32 +0200

    📲 Added 2.00 GUI base classes for NG_Import.

--- Tools/NavGridPP/NG_importGUI.hpp
+++ Tools/NavGridPP/NG_importGUI.hpp
@@ -1,7 +1,92 @@
+/*  Enable when `import` becomes avalable (projected to be Arma 3 - 2.02)
 import RscEditMulti;
 import RscText;
 import RscFrame;
 import RscButton;
+*/
+
+// Delete CT, ST and Rsc base classes when `import` becomes avalable (projected to be Arma 3 2.02)
+#define CT_STATIC           0
+#define CT_BUTTON           1
+#define CT_EDIT             2
+#define ST_LEFT           0x00
+#define ST_CENTER         0x02
+#define ST_MULTI          0x10
+#define ST_FRAME          0x40
+
+class CommonRsc {
+    deletable = 0;
+    fade = 0;
+    access = 1;
+    colorBackground[] = {0,0,0,0};
+    colorText[] = {0.95,0.95,0.95,1};
+    colorDisabled[] = {1,1,1,0.25};
+    font = "RobotoCondensed";
+    text = "";
+    shadow = 2;
+    idc = -1;
+    type = CT_STATIC;
+};
+class RscText : CommonRsc {
+    fixedWidth = 0;
+    style = ST_LEFT;
+    colorShadow[] = {0,0,0,0.5};
+    SizeEx = 0.05;
+    linespacing = 1;
+    tooltipColorText[] = {1,1,1,1};
+    tooltipColorBox[] = {1,1,1,1};
+    tooltipColorShade[] = {0,0,0,0.65};
+};
+class RscEditMulti : RscText
+{
+    type = CT_EDIT;
+    colorSelection[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.13])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.54])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.21])",1};
+    autocomplete = "";
+    style = ST_MULTI;
+    canModify = 1;
+};
+class RscFrame : CommonRsc {
+    style = ST_FRAME;
+};
+class RscButton : CommonRsc {
+    type = CT_BUTTON;
+    colorBackgroundDisabled[] = {0,0,0,0.5};
+    colorBackgroundActive[] = {0,0,0,1};
+    colorFocused[] = {0,0,0,1};
+    colorShadow[] = {0,0,0,0};
+    colorBorder[] = {0,0,0,1};
+    soundEnter[] =
+    {
+        "\A3\ui_f\data\sound\RscButton\soundEnter",
+        0.09,
+        1
+    };
+    soundPush[] =
+    {
+        "\A3\ui_f\data\sound\RscButton\soundPush",
+        0.09,
+        1
+    };
+    soundClick[] =
+    {
+        "\A3\ui_f\data\sound\RscButton\soundClick",
+        0.09,
+        1
+    };
+    soundEscape[] =
+    {
+        "\A3\ui_f\data\sound\RscButton\soundEscape",
+        0.09,
+        1
+    };
+    style = ST_CENTER;
+    offsetX = 0;
+    offsetY = 0;
+    offsetPressedX = 0;
+    offsetPressedY = 0;
+    borderSize = 0;
+};
+// End of CT, ST and Rsc base classes
 
 #define NG_FRAME_X (0.05 * safezoneW + safezoneX)
 #define NG_FRAME_Y (0.1 * safezoneH + safezoneY)
@@ -40,7 +125,6 @@ class A3A_NG_import_dialogue {
         };
 
         class EditBox : RscEditMulti {
-            font = "RobotoCondensed";
             text = "Ctrl+A then Ctrl+V";
 
             size = 0.55;
